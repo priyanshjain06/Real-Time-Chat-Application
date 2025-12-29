3e8a504755d00084ad83854ffe65233b1a0e2326
Priyansh Jain <priyansh01jain@gmail.com>
2025-12-29 14:56:52 +0530

    added comment!

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-//REVIEW  
+
+//REVIEW   for deployement ! 
+
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-  withCredentials: true,
+  withCredentials: true, //Sends cookies with every request  and allow backend to read token in cookie (req.cookie) , session cookie !!
 });
